f4477943fe5ca02b4f65e339014f96725da9531f
Bart Naudts <Bart.Naudts@codelaser.io>
2024-07-02 09:35:37 +0200

    what about delays?

--- e2immu-shallow-analyzer/src/test/java/org/e2immu/analyzer/shallow/analyzer/TestParseAnalyzeWrite.java
+++ e2immu-shallow-analyzer/src/test/java/org/e2immu/analyzer/shallow/analyzer/TestParseAnalyzeWrite.java
@@ -8,6 +8,7 @@ import org.e2immu.language.cst.api.info.FieldInfo;
 import org.e2immu.language.cst.api.info.MethodInfo;
 import org.e2immu.language.cst.api.info.TypeInfo;
 import org.e2immu.language.cst.api.runtime.Runtime;
+import org.e2immu.language.cst.api.type.ParameterizedType;
 import org.e2immu.language.cst.impl.analysis.PropertyImpl;
 import org.e2immu.language.cst.impl.analysis.ValueImpl;
 import org.e2immu.language.inspection.api.integration.JavaInspector;
@@ -47,6 +48,7 @@ public class TestParseAnalyzeWrite {
         ShallowTypeAnalyzer shallowTypeAnalyzer = new ShallowTypeAnalyzer(annotatedApiParser);
         ShallowMethodAnalyzer shallowMethodAnalyzer = new ShallowMethodAnalyzer(annotatedApiParser);
         List<TypeInfo> types = annotatedApiParser.types();
+        // FIXME we must sort these types according to the hierarchy, or use a system of delays
         for (TypeInfo typeInfo : types) {
             shallowTypeAnalyzer.analyze(typeInfo);
         }
@@ -106,8 +108,17 @@ public class TestParseAnalyzeWrite {
         TypeInfo typeInfo = compiledTypesManager.get(Collection.class);
         assertTrue(typeInfo.isInterface());
         assertFalse(typeInfo.isAtLeastImmutableHC());
+        assertSame(MUTABLE, typeInfo.analysis().getOrDefault(IMMUTABLE_TYPE, MUTABLE));
         assertSame(DEPENDENT, typeInfo.analysis().getOrDefault(INDEPENDENT_TYPE, DEPENDENT));
-        assertTrue(typeInfo.analysis().getOrDefault(CONTAINER_TYPE, ValueImpl.BoolImpl.FALSE).isTrue());
+        assertSame(TRUE, typeInfo.analysis().getOrDefault(CONTAINER_TYPE, FALSE));
+    }
+
+    @Test
+    public void testCollectionAdd() {
+        TypeInfo typeInfo = compiledTypesManager.get(Collection.class);
+        MethodInfo add = typeInfo.findUniqueMethod("add", 1);
+        assertTrue(add.overrides().isEmpty());
+        assertSame(TRUE, add.analysis().getOrDefault(MODIFIED_METHOD, FALSE));
     }
 
     @Test
@@ -115,8 +126,13 @@ public class TestParseAnalyzeWrite {
         TypeInfo typeInfo = compiledTypesManager.get(List.class);
         assertTrue(typeInfo.isInterface());
         assertFalse(typeInfo.isAtLeastImmutableHC());
+        ParameterizedType collection = typeInfo.interfacesImplemented().get(0);
+        assertEquals("Type java.util.Collection<E>", collection.toString());
+        assertEquals("E=TP#0 in List", collection.parameters().get(0).typeParameter().toString());
+
+        assertSame(MUTABLE, typeInfo.analysis().getOrDefault(IMMUTABLE_TYPE, MUTABLE));
         assertSame(DEPENDENT, typeInfo.analysis().getOrDefault(INDEPENDENT_TYPE, DEPENDENT));
-        assertTrue(typeInfo.analysis().getOrDefault(CONTAINER_TYPE, ValueImpl.BoolImpl.FALSE).isTrue());
+        assertSame(TRUE, typeInfo.analysis().getOrDefault(CONTAINER_TYPE, FALSE));
     }
 
     @Test
@@ -124,6 +140,8 @@ public class TestParseAnalyzeWrite {
         TypeInfo typeInfo = compiledTypesManager.get(List.class);
         MethodInfo add = typeInfo.findUniqueMethod("add", 1);
         assertEquals(1, add.overrides().size());
+        MethodInfo override = add.overrides().stream().findFirst().orElseThrow();
+        assertEquals("java.util.Collection.add(E)", override.fullyQualifiedName());
         assertSame(TRUE, add.analysis().getOrDefault(MODIFIED_METHOD, FALSE));
     }
 
