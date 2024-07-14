312e74ec57771fb3b063418a69e02039f8cc3cd5
Bart Naudts <Bart.Naudts@codelaser.io>
2024-07-14 15:49:45 +0200

    composer almost ok

--- e2immu-shallow-analyzer/src/main/java/org/e2immu/analyzer/shallow/analyzer/Composer.java
+++ e2immu-shallow-analyzer/src/main/java/org/e2immu/analyzer/shallow/analyzer/Composer.java
@@ -15,14 +15,19 @@
 package org.e2immu.analyzer.shallow.analyzer;
 
 
-import org.e2immu.language.cst.api.info.Info;
-import org.e2immu.language.cst.api.info.TypeInfo;
+import org.e2immu.language.cst.api.element.CompilationUnit;
+import org.e2immu.language.cst.api.expression.Expression;
+import org.e2immu.language.cst.api.info.*;
 import org.e2immu.language.cst.api.output.Formatter;
-import org.e2immu.language.cst.api.output.FormattingOptions;
 import org.e2immu.language.cst.api.output.OutputBuilder;
 import org.e2immu.language.cst.api.runtime.Runtime;
+import org.e2immu.language.cst.api.statement.Block;
+import org.e2immu.language.cst.api.statement.Statement;
+import org.e2immu.language.cst.api.type.ParameterizedType;
+import org.e2immu.language.cst.impl.info.TypePrinter;
 import org.e2immu.language.cst.print.FormatterImpl;
 import org.e2immu.language.cst.print.FormattingOptionsImpl;
+import org.e2immu.util.internal.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,10 +36,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -72,168 +74,136 @@ public class NameOfPackageWithoutDots {
 public class Composer {
     private static final Logger LOGGER = LoggerFactory.getLogger(Composer.class);
     private final Runtime runtime;
-    private final String destinationDirectory;
+    private final String destinationPackage;
     private final Predicate<Info> predicate;
 
-    public Composer(Runtime runtime, String destinationDirectory, Predicate<Info> predicate) {
+    public Composer(Runtime runtime,
+                    String destinationPackage,
+                    Predicate<Info> predicate) {
         this.runtime = runtime;
-        this.destinationDirectory = destinationDirectory;
+        this.destinationPackage = destinationPackage;
         this.predicate = predicate;
     }
 
-/*
     public Collection<TypeInfo> compose(Collection<TypeInfo> primaryTypes) {
-        Map<String, TypeInspectionImpl.Builder> buildersPerPackage = new HashMap<>();
+        Map<String, TypeInfo> typesPerPackage = new HashMap<>();
         for (TypeInfo primaryType : primaryTypes) {
             assert primaryType.isPrimaryType();
             String packageName = primaryType.packageName();
-            TypeInspectionImpl.Builder builder = buildersPerPackage.computeIfAbsent(packageName,
-                    this::newPackageTypeBuilder);
-            appendType(primaryType, builder, true);
+            TypeInfo ti = typesPerPackage.computeIfAbsent(packageName, this::newPackageType);
+            appendType(primaryType, ti, true);
         }
-        return buildersPerPackage.values().stream()
-                .map(builder -> {
-                    TypeInspection inspection = builder.setFunctionalInterface(null).build(typeMap);
-                    TypeInfo typeInfo = inspection.typeInfo();
-                    typeInfo.typeInspection.set(inspection);
-                    return typeInfo;
-                })
-                .toList();
+        List<TypeInfo> allTypes = typesPerPackage.values().stream().toList();
+        allTypes.forEach(t -> t.builder().commit());
+        return allTypes;
     }
 
-    private void appendType(TypeInfo primaryType, TypeInspectionImpl.Builder packageBuilder, boolean topLevel) {
+    private void appendType(TypeInfo primaryType, TypeInfo packageType, boolean topLevel) {
+        packageType.builder().setSingleAbstractMethod(null);
         if (!acceptTypeOrAnySubType(primaryType)) return;
-        if (!primaryType.typeInspection.isSet()) {
-            LOGGER.warn("Ignoring {} in composer, no type inspection", primaryType.fullyQualifiedName);
-            return;
-        }
-        TypeInspection typeInspection = primaryType.typeInspection.get(primaryType.fullyQualifiedName);
-        TypeInspectionImpl.Builder typeBuilder = newTypeBuilder(packageBuilder.typeInfo(), typeInspection, topLevel);
+        TypeInfo newType = createType(packageType, primaryType, topLevel);
+        newType.builder().setAccess(runtime.accessPackage()).setSingleAbstractMethod(null);
 
-        for (TypeInfo subType : typeInspection.subTypes()) {
-            appendType(subType, typeBuilder, false);
+        for (TypeInfo subType : primaryType.subTypes()) {
+            appendType(subType, packageType, false);
         }
-        for (FieldInfo fieldInfo : typeInspection.fields()) {
-            if (fieldInfo.fieldInspection.get().isPublic() && predicate.test(fieldInfo)) {
-                typeBuilder.addField(createField(fieldInfo, typeBuilder.typeInfo()));
+        for (FieldInfo fieldInfo : primaryType.fields()) {
+            if (fieldInfo.access().isPublic() && predicate.test(fieldInfo)) {
+                newType.builder().addField(createField(fieldInfo, newType));
             }
         }
-        for (MethodInfo constructor : typeInspection.constructors()) {
+        for (MethodInfo constructor : primaryType.constructors()) {
             if (predicate.test(constructor)) {
-                typeBuilder.addMethod(createMethod(constructor, typeBuilder.typeInfo()));
+                newType.builder().addMethod(createMethod(constructor, newType));
             }
         }
-        for (MethodInfo methodInfo : typeInspection.methods()) {
+        for (MethodInfo methodInfo : primaryType.methods()) {
             if (predicate.test(methodInfo)) {
-                typeBuilder.addMethod(createMethod(methodInfo, typeBuilder.typeInfo()));
+                newType.builder().addMethod(createMethod(methodInfo, newType));
             }
         }
-
-        TypeInspection builtType = typeBuilder.setFunctionalInterface(null)
-                .setAccess(Inspection.Access.PACKAGE)
-                .build(typeMap);
-        TypeInfo typeInfo = builtType.typeInfo();
-        typeInfo.typeInspection.set(builtType);
-        packageBuilder.addSubType(typeInfo);
+        packageType.builder().addSubType(newType);
     }
 
     private boolean acceptTypeOrAnySubType(TypeInfo typeInfo) {
-        if (predicate().test(typeInfo)) return true;
-        if (typeInfo.typeInspection.isSet()) {
-            TypeInspection inspection = typeInfo.typeInspection.get(typeInfo.fullyQualifiedName);
-            return inspection.subTypes().stream().anyMatch(this::acceptTypeOrAnySubType);
-        }
-        LOGGER.warn("Ignoring type {} in composer", typeInfo.fullyQualifiedName);
-        return false;
+        if (predicate.test(typeInfo)) return true;
+        return typeInfo.subTypes().stream().anyMatch(this::acceptTypeOrAnySubType);
     }
 
     private FieldInfo createField(FieldInfo fieldInfo, TypeInfo owner) {
-        FieldInspection inspection = fieldInfo.fieldInspection.get();
-        FieldInfo newField = new FieldInfo(fieldInfo.getIdentifier(), fieldInfo.type, fieldInfo.name, owner);
-        FieldInspectionImpl.Builder builder = new FieldInspectionImpl.Builder(fieldInfo);
-        inspection.getModifiers()
-                .stream().filter(m -> m != FieldModifier.PUBLIC).forEach(builder::addModifier);
-        if (inspection.getModifiers().contains(FieldModifier.FINAL)) {
-            TypeInfo bestType = fieldInfo.type.bestTypeInfo();
-            builder.setInspectedInitialiserExpression(bestType == null ?
-                    NullConstant.NULL_CONSTANT : ConstantExpression.nullValue(typeMap.getPrimitives(),
-                    fieldInfo.getIdentifier(), bestType));
+        FieldInfo newField = runtime.newFieldInfo(fieldInfo.name(), fieldInfo.isStatic(), fieldInfo.type(), owner);
+        fieldInfo.modifiers()
+                .stream().filter(m -> !m.isPublic()).forEach(newField.builder()::addFieldModifier);
+        if (fieldInfo.isFinal()) {
+            TypeInfo bestType = fieldInfo.type().bestTypeInfo();
+            newField.builder().setInitializer(bestType == null ? runtime.nullConstant() : runtime.nullValue(bestType));
         }
-        newField.fieldInspection.set(builder
-                .setAccess(Inspection.Access.PACKAGE)
-                .build(typeMap));
+        newField.builder().setAccess(runtime.accessPackage()).commit();
         return newField;
     }
 
     private MethodInfo createMethod(MethodInfo methodInfo, TypeInfo owner) {
-        MethodInspection methodInspection = methodInfo.methodInspection.get();
-        MethodInspectionImpl.Builder builder;
+        MethodInfo newMethod;
         if (methodInfo.isConstructor()) {
-            builder = new MethodInspectionImpl.Builder(owner, MethodInfo.MethodType.CONSTRUCTOR);
+            newMethod = runtime.newConstructor(owner);
         } else {
-            builder = new MethodInspectionImpl.Builder(owner, methodInfo.name, MethodInfo.MethodType.METHOD);
+            MethodInfo.MethodType methodType = methodInfo.isStatic() ? runtime.methodTypeStaticMethod()
+                    : runtime.methodTypeMethod();
+            newMethod = runtime.newMethod(owner, methodInfo.name(), methodType);
         }
-
-        if (methodInfo.isStatic()) builder.addModifier(MethodModifier.STATIC);
-
-        ParameterizedType returnType = methodInspection.getReturnType();
-        builder.setReturnType(returnType);
+        ParameterizedType returnType = methodInfo.returnType();
+        newMethod.builder()
+                .setAccess(runtime.accessPackage())
+                .setReturnType(returnType);
         if (methodInfo.hasReturnValue()) {
-            Expression defaultReturnValue = ConstantExpression.nullValue(typeMap().getPrimitives(),
-                    methodInfo.getIdentifier(), returnType.typeInfo);
-            Statement returnStatement = new ReturnStatement(methodInfo.identifier, defaultReturnValue);
-            Block block = new Block.BlockBuilder(Identifier.generate("compose block")).addStatement(returnStatement).build();
-            builder.setInspectedBlock(block);
+            Expression defaultReturnValue = runtime.nullValue(returnType.typeInfo());
+            Statement returnStatement = runtime.newReturnStatement(defaultReturnValue);
+            Block block = runtime.newBlockBuilder().addStatement(returnStatement).build();
+            newMethod.builder().setMethodBody(block);
+        } else {
+            newMethod.builder().setMethodBody(runtime.newBlockBuilder().build());
         }
-        for (ParameterInfo p : methodInspection.getParameters()) {
-            ParameterInspection.Builder newParameterBuilder = builder.newParameterInspectionBuilder
-                    (p.identifier, p.parameterizedType, p.name, p.index);
-            if (p.parameterInspection.get().isVarArgs()) {
-                newParameterBuilder.setVarArgs(true);
-            }
-            builder.addParameter(newParameterBuilder);
+        for (ParameterInfo p : methodInfo.parameters()) {
+            ParameterInfo pi = newMethod.builder().addParameter(p.name(), p.parameterizedType());
+            pi.builder().setVarArgs(p.isVarArgs()).commit();
         }
-        MethodInfo newMethod = builder
-                .setAccess(Inspection.Access.PACKAGE)
-                .build(InspectionProvider.DEFAULT).getMethodInfo();
-        newMethod.methodResolution.set(new MethodResolution.Builder().build());
         return newMethod;
     }
 
-    private TypeInspectionImpl.Builder newTypeBuilder(TypeInfo packageType, TypeInspection typeToCopy, boolean topLevel) {
-        String typeName = typeToCopy.typeInfo().simpleName;
-        TypeInfo typeInfo = new TypeInfo(packageType, topLevel ? typeName + "$" : typeName);
-        TypeInspectionImpl.Builder builder = new TypeInspectionImpl.Builder(typeInfo, BY_HAND_WITHOUT_STATEMENTS);
-        builder.noParent(typeMap.getPrimitives())
-                .setTypeNature(TypeNature.CLASS)
-                .addTypeModifier(TypeModifier.STATIC);
-        return builder;
+    private TypeInfo createType(TypeInfo packageType, TypeInfo typeToCopy, boolean topLevel) {
+        String typeName = typeToCopy.simpleName();
+        TypeInfo typeInfo = runtime.newTypeInfo(packageType, topLevel ? typeName + "$" : typeName);
+        typeInfo.builder().setParentClass(runtime.objectParameterizedType())
+                .setTypeNature(runtime.typeNatureClass())
+                .setAccess(runtime.accessPackage());
+        return typeInfo;
     }
 
-    private TypeInspectionImpl.Builder newPackageTypeBuilder(String packageName) {
+    private TypeInfo newPackageType(String packageName) {
         String camelCasePackageName = convertToCamelCase(packageName);
-        TypeInfo typeInfo = new TypeInfo(destinationPackage, camelCasePackageName);
-        TypeInspectionImpl.Builder builder = new TypeInspectionImpl.Builder(typeInfo, BY_HAND_WITHOUT_STATEMENTS);
-        builder.noParent(typeMap.getPrimitives())
-                .setTypeNature(TypeNature.CLASS)
-                .addTypeModifier(TypeModifier.PUBLIC);
-        FieldInfo packageField = new FieldInfo(Identifier.generate("PACKAGE NAME"), typeMap.getPrimitives().stringParameterizedType(),
-                "PACKAGE_NAME", typeInfo);
-        FieldInspectionImpl.Builder packageFieldInspectionBuilder = new FieldInspectionImpl.Builder(packageField);
-        packageFieldInspectionBuilder
-                .addModifier(FieldModifier.FINAL)
-                .addModifier(FieldModifier.STATIC)
-                .addModifier(FieldModifier.PUBLIC)
-                .setAccess(Inspection.Access.PUBLIC)
-                .setInspectedInitialiserExpression(new StringConstant(typeMap.getPrimitives(), packageName));
-        packageField.fieldInspection.set(packageFieldInspectionBuilder.build(typeMap));
+        CompilationUnit compilationUnit = runtime.newCompilationUnitBuilder().setPackageName(destinationPackage).build();
+        TypeInfo typeInfo = runtime.newTypeInfo(compilationUnit, camelCasePackageName);
+        TypeInfo.Builder builder = typeInfo.builder();
+        builder.setTypeNature(runtime.typeNatureClass())
+                .setParentClass(runtime.objectParameterizedType())
+                .addTypeModifier(runtime.typeModifierPublic())
+                .setAccess(runtime.accessPublic());
+        FieldInfo packageField = runtime.newFieldInfo("PACKAGE_NAME", true,
+                runtime.stringParameterizedType(), typeInfo);
+        packageField.builder()
+                .addFieldModifier(runtime.fieldModifierFinal())
+                .addFieldModifier(runtime.fieldModifierStatic())
+                .addFieldModifier(runtime.fieldModifierPublic())
+                .setAccess(runtime.accessPublic())
+                .setInitializer(runtime.newStringConstant(packageName))
+                .commit();
         builder.addField(packageField);
-        return builder;
+        return typeInfo;
     }
 
     static String convertToCamelCase(String packageName) {
         String[] components = packageName.split("\\.");
-        return Arrays.stream(components).map(StringUtil::capitalise).collect(Collectors.joining());
+        return Arrays.stream(components).map(StringUtil::capitalize).collect(Collectors.joining());
     }
 
     public void write(Collection<TypeInfo> apiTypes, String writeAnnotatedAPIsDir) throws IOException {
@@ -241,9 +211,9 @@ public class Composer {
         if (base.mkdirs()) {
             LOGGER.info("Created annotated API destination folder {}", base);
         }
+        Formatter formatter = new FormatterImpl(runtime, FormattingOptionsImpl.DEFAULT);
         for (TypeInfo apiType : apiTypes) {
-            OutputBuilder outputBuilder = apiType.print();
-            Formatter formatter = new FormatterImpl(runtime, FormattingOptionsImpl.DEFAULT);
+            assert apiType.isPrimaryType() && apiType.hasBeenInspected();
 
             String convertedPackage = apiType.packageName().replace(".", "/");
             File directory = new File(base, convertedPackage);
@@ -253,8 +223,9 @@ public class Composer {
             File outputFile = new File(directory, apiType.simpleName() + ".java");
             try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(outputFile),
                     StandardCharsets.UTF_8)) {
+                OutputBuilder outputBuilder = apiType.print(null);
                 outputStreamWriter.write(formatter.write(outputBuilder));
             }
         }
-    }*/
+    }
 }
---
+++ e2immu-shallow-analyzer/src/test/java/org/e2immu/analyzer/shallow/analyzer/TestComposer.java
@@ -0,0 +1,66 @@
+package org.e2immu.analyzer.shallow.analyzer;
+
+
+import ch.qos.logback.classic.Level;
+import org.e2immu.language.cst.api.info.TypeInfo;
+import org.e2immu.language.cst.api.output.Formatter;
+import org.e2immu.language.cst.api.output.OutputBuilder;
+import org.e2immu.language.cst.print.FormatterImpl;
+import org.e2immu.language.cst.print.FormattingOptionsImpl;
+import org.e2immu.language.inspection.api.integration.JavaInspector;
+import org.e2immu.language.inspection.integration.JavaInspectorImpl;
+import org.e2immu.language.inspection.resource.InputConfigurationImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestComposer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestComposer.class);
+    public static final String TEST_DIR = "build/testAAPI";
+
+    @BeforeAll
+    public static void beforeAll() {
+        ((ch.qos.logback.classic.Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)).setLevel(Level.INFO);
+        ((ch.qos.logback.classic.Logger) LoggerFactory.getLogger("org.e2immu.analyzer.shallow")).setLevel(Level.DEBUG);
+    }
+
+    @Test
+    public void test() throws IOException {
+        InputConfigurationImpl.Builder inputConfigurationBuilder = new InputConfigurationImpl.Builder()
+                .addSources("none")
+                .addClassPath("jmods/java.base.jmod");
+
+        JavaInspector javaInspector = new JavaInspectorImpl();
+        javaInspector.initialize(inputConfigurationBuilder.build());
+
+        Composer composer = new Composer(javaInspector.runtime(), "org.e2immu.testannotatedapi", w -> true);
+        List<TypeInfo> primaryTypes = javaInspector.compiledTypesManager()
+                .typesLoaded().stream().filter(TypeInfo::isPrimaryType).toList();
+        LOGGER.info("Have {} primary types loaded", primaryTypes.size());
+        Collection<TypeInfo> apiTypes = composer.compose(primaryTypes);
+
+        Path defaultDestination = Path.of(TEST_DIR);
+        defaultDestination.toFile().mkdirs();
+        try (Stream<Path> walk = Files.walk(defaultDestination)) {
+            walk.sorted(Comparator.reverseOrder())
+                    .map(Path::toFile)
+                    .peek(System.out::println)
+                    .forEach(File::delete);
+        }
+
+        composer.write(apiTypes, TEST_DIR);
+    }
+}
