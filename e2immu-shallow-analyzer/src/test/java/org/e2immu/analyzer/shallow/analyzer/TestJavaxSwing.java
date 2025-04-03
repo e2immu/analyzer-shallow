815d885f85dfc67950c84710172dd6123bf280c6
Bart Naudts <Bart.Naudts@codelaser.io>
2025-04-03 07:50:45 +0200

    fixed test javax swing

--- e2immu-shallow-analyzer/src/test/java/org/e2immu/analyzer/shallow/analyzer/TestJavaxSwing.java
+++ e2immu-shallow-analyzer/src/test/java/org/e2immu/analyzer/shallow/analyzer/TestJavaxSwing.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import javax.swing.*;
 
 import static org.e2immu.language.cst.impl.analysis.PropertyImpl.COMMUTABLE_METHODS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestJavaxSwing extends CommonTest {
@@ -39,7 +40,9 @@ public class TestJavaxSwing extends CommonTest {
         TypeInfo typeInfo = compiledTypesManager.getOrLoad(JTable.class);
         MethodInfo methodInfo = typeInfo.findUniqueMethod("setDefaultRenderer", 2);
         assertTrue(methodInfo.isModifying());
-        testCommutable(methodInfo);
+        Value.CommutableData cd = methodInfo.analysis().getOrNull(COMMUTABLE_METHODS, ValueImpl.CommutableData.class);
+        assertEquals("", cd.par());
+        assertEquals("class,0", cd.seq());
     }
 
     @Test
